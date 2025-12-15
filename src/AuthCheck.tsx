6703751a3392bd9feeb5ba75c605a765db61db48
rikotacards <maxhsu1@gmail.com>
2025-12-15 07:00:57 -0800

    add login page.

--- src/AuthCheck.tsx
+++ src/AuthCheck.tsx
@@ -1,14 +1,16 @@
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import { useAuth } from "./providers/contexts/authContext";
 import { LinearProgress } from "@mui/material";
 
 export const AuthCheck: React.FC = () => {
   const auth = useAuth();
   const nav = useNavigate();
+  const l = useLocation();
+  const page = l.pathname.slice(0);
   if (auth.loading) {
     return <LinearProgress />;
   }
-  if (!auth.user) {
+  if (!auth.user && page !== "login") {
     nav("login");
   }
   return <Outlet />;
